21a61e5e9464337ee5d91261fc8f208ef8878c1f
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2022-10-10 09:42:22 -0300

    Mudança no código fonte
    Adicionei tipos primitivos e printei em uma nova linha para testar

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int idade = 28;
+        double salarioMinimo = 1500;
+        salarioMinimo = 2000;
+        long cpf = 98765432109L;
+        System.out.println(idade);
+        System.out.println(salarioMinimo);
+        System.out.println(cpf);
     }
 }
