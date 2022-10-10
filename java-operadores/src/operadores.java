5059b629ed3c1d8a9d41331de03df1592df49377
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2022-10-10 10:56:10 -0300

    Regras de concatenação
    Linhas de concatenação e comentários a respeito sobre o tema.

--- java-operadores/src/operadores.java
+++ java-operadores/src/operadores.java
@@ -1,5 +1,21 @@
 public class operadores {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println(nomeCompleto);
+
+        String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1"; // aqui vai somar os 3 primeiros "1" + o caracters 1, vai dar "31"
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1; // aqui como começa com uma operação, porem o proximo ele concatenou com "1" em seguida as operações não funcional mais, dando o resultado de "1111"
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1); // Priorizou o que está em parênteses, para depois fazer a concatenação dando o resultado de "13"
+
+        System.out.println(concatenacao);
+
     }
 }
