6741f7726a388df3a9da5d148ace0db19d6757ec
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2022-10-10 11:40:54 -0300

    Operador Ternario
    Adicionado linhas de codigo e comentarios para exemplificar como funciona operadores ternários

---
+++ java-operadores/src/OperadorTernario.java
@@ -0,0 +1,33 @@
+public class OperadorTernario {
+
+    public static void main(String[] args) {
+        
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        String resultado = a == b ? "verdadeiro" : "falso"; // aqui é o resultado abreviado. 
+        //o "?" significa "IF" o ":" significa "ELSE" 
+
+
+
+        // Abaixo você tem o metodo tradicional
+
+       // if (a == b)
+       //     resultado = "verdadeiro"; 
+       // else    
+       //     resultado = "falso";
+
+        System.out.println(resultado);
+
+    
+
+    }
+
+
+
+
+
+
+}
