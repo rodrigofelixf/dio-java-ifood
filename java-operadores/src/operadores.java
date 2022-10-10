318ae76c3534d0e2b5190bbbc7b11f298d4c21da
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2022-10-10 16:14:20 -0300

    Atualização de Classe
    Atualizei a classe e deixei padrão.

--- java-operadores/src/operadores.java
+++ java-operadores/src/operadores.java
@@ -1,4 +1,4 @@
-public class operadores {
+public class Operadores {
     public static void main(String[] args) throws Exception {
         String nomeCompleto = "LINGUAGEM" + "JAVA";
         System.out.println(nomeCompleto);
