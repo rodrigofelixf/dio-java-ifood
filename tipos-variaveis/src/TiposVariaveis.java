c0bcb71f30bf397010d2fb1c9a8acb6201d7576f
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2022-10-10 10:03:55 -0300

    Variáveis e Constantes
    Adicionei linhas e comentários sobre as constantes.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -10,7 +10,15 @@ public class TiposVariaveis {
     //Na linha abaixo esto testando as regras dos numeros primitivos
         short numeroCurto = 1;
         int numeroNormal = numeroCurto; // o tipo primitivo "int" pode receber um tipo "short"
-        short numeroCurto = numeroNormal; // mas ao contrario não funciona. O short não recebe um tipo "int"
+        //short numeroCurto = numeroNormal; // mas ao contrario não funciona. O short não recebe um tipo 
+
+        final String BR = "Brasil";
+        
+        // variaves são imutáveis quando recebem a constante "final" antes do tipo (variavel tem que ser em CALXA ALTA quando for constante)
+
+        BR = "EUA"; // tentei declarar que a váriavel BR recebesse a String "EUA", deu erro porque ela é imutável
+        System.out.println(BR);
+
 
     }
 }
