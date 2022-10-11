32cb19e1d4aef4d4dd130befc81713ce7ec093c0
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2022-10-11 16:39:53 -0300

    Controle de If e Else if

---
+++ java-controle-fluxos/src/Exercicio.java
@@ -0,0 +1,22 @@
+public class Exercicio {
+    public static void main(String[] args) {
+        
+        ifSemFlecha(1);
+
+    }
+    private static void ifSemFlecha(int mes) {
+
+   
+    if (mes == 1)
+        System.out.println("Janeiro");
+    
+    else if (mes == 2)
+        System.out.println("Fevereiro");
+    
+    }
+
+    
+}
+
+    
+
