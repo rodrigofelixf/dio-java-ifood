c1da9ddd0787630bffbcf6890a2d461faa4c215d
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2022-10-13 14:13:14 -0300

    Atualização de teste
    testando atualizando. Desconsiderar

--- java-controle-fluxos/src/Exercicio.java
+++ java-controle-fluxos/src/Exercicio.java
@@ -1,22 +1,10 @@
 public class Exercicio {
     public static void main(String[] args) {
         
-        ifSemFlecha(1);
+     if ((10 < 15) && true)
+        System.out.println("verdade");
 
     }
-    private static void ifSemFlecha(int mes) {
 
-   
-    if (mes == 1)
-        System.out.println("Janeiro");
-    
-    else if (mes == 2)
-        System.out.println("Fevereiro");
-    
-    }
-
-    
 }
-
-    
 
