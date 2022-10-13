4eebb13337fd29f5e84e21f9a37f15ca88f6802a
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2022-10-13 14:27:02 -0300

    Operadores Ternarios
    Adicionado linhas de códigos para estudos de Operadores Ternarios

---
+++ OperadorTernario/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int n1, n2, r;
+        n1 = 4;
+        n2 = 8;
+        r = (n1 > n2) ? 0:1; // aqui posso usar operações também para determinar resultado
+        System.out.println(r);
+    }
+}
