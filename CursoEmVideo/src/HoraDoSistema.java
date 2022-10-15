d0683177284608609dcc880b3066687c41d3e8d9
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2022-10-15 16:57:16 -0300

    Adicionando Projeto CursoEmVideo
    Adicionado projetos e exercicios do curso de Java Básico do Curso em vídeo.

---
+++ CursoEmVideo/src/HoraDoSistema.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class HoraDoSistema {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("A hora do sistema é " + date.toString());
+        
+    }
+    
+}
